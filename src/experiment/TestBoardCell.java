3196cb6c0ec8fba495c592fcd288d3030a7d2a6e
Sammy Flemington <samflemington@gmail.com>
2022-02-28 20:01:25 -0700

    Completed testboardcell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,42 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
+import java.util.HashSet;
 
+public class TestBoardCell {
+	private int row;
+	private int col;
+	private boolean isRoom = true;
+	private boolean isOccupied = false;
+	private Set<TestBoardCell> adjList = new HashSet<TestBoardCell> ();
+	
+	public TestBoardCell(int r, int c) {
+		row = r;
+		col = c;
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		adjList.add(cell);
+	}
+	
+	Set<TestBoardCell> getAdjList(){
+		return adjList;
+	}
+	
+	void setRoom(boolean b) {
+		isRoom = b;
+	}
+	
+	boolean isRoom() {
+		return isRoom;
+	}
+	
+	void setOccupied(boolean b) {
+		isOccupied = b;
+	}
+	
+	boolean getOccupied() {
+		return isOccupied;
+	}
 }
+
