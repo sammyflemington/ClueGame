c50069a6d4eb1c0d78771794b5b3a94180852294
Eliot Edwards <eedwards@mines.edu>
2022-02-28 20:55:12 -0700

    Added tests to BoardTestsExp.java

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -22,11 +22,29 @@ public class BoardTestsExp {
 		
 		TestBoardCell cell = board.get(1,1);
 		Set<TestBoardCell> testList = cell.getAdjList();
+		
+		// Middle
 		assertTrue(testList.contains(board.getCell(0,1)));
 		assertTrue(testList.contains(board.getCell(1,0)));
 		assertTrue(testList.contains(board.getCell(2,1)));
 		assertTrue(testList.contains(board.getCell(1,2)));
+		assertEquals(4, testList.size());
+		
+		// Corner
+		cell = board.get(0,0);
+		testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(0,1)));
+		assertTrue(testList.contains(board.getCell(1,0)));
 		assertEquals(2, testList.size());
+		
+		// Edge
+		cell = board.get(9,21);
+		testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(10,21)));
+		assertTrue(testList.contains(board.getCell(8,21)));
+		assertTrue(testList.contains(board.getCell(9,20)));
+		assertEquals(3, testList.size());
+		
 		
 	}
 	
