38deb0b77c215a331a857a51f21b423b9badc243
Eliot Edwards <eedwards@mines.edu>
2022-03-03 20:12:34 -0700

    Worked on BoardCell

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,26 @@
 package clueGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import experiment.TestBoardCell;
+
 public class BoardCell {
 
+	static private int COL;
+	static private int ROW;
+	static private char initial;
+	
+	DoorDirection doorDirection;
+	
+	private boolean roomLabel;
+	private boolean roomCenter;
+	private char 	secretPassage;
+	
+	private Set<BoardCell> adjList = new HashSet<BoardCell>();
+	
+	public void addAdj(BoardCell cell) {
+		// do something
+	}
+	
 }
