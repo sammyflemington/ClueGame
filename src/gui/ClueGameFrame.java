aeb7a195acd2cf97dedd76cced6e18141f12af5e
Eliot Edwards <eedwards@mines.edu>
2022-04-15 13:10:20 -0600

    Added splash screen

--- src/gui/ClueGameFrame.java
+++ src/gui/ClueGameFrame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import clueGame.Board;
 import clueGame.Card;
@@ -22,17 +24,26 @@ public class ClueGameFrame extends JFrame{
 		super();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
 		board.initialize();
-		add(board, BorderLayout.CENTER);
+		add(board, BorderLayout.CENTER);			// put board in center to start
 		cardPanel = new CardPanel();
 		gameControlPanel = new GameControlPanel();
-		add(cardPanel, BorderLayout.EAST);
-		add(gameControlPanel, BorderLayout.SOUTH);
+		add(cardPanel, BorderLayout.EAST);			// put card panel on right, shifts board over
+		add(gameControlPanel, BorderLayout.SOUTH);	// put game control panel on bottom
 		setTitle("Really Good Lookin' Clue!");
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		ClueGameFrame game = new ClueGameFrame();
 		HumanPlayer p = board.getHumanPlayer();
+		
+		// Splash screen
+		// arguments: frame (JFrame), message (String), frame title (string), icon type
+		JFrame splash = new JFrame("");
+		String text = "You are " + board.getHumanPlayer().getName() + ".<br>" + "Can you find the killer<br>" + "before the Computer players?";
+		JLabel label = new JLabel("<html><p align=center>" + text);
+        //label.setHorizontalAlignment(JLabel.CENTER);		-- this may be redundant
+		JOptionPane.showMessageDialog(splash, label, "Welcome to Clue!", JOptionPane.INFORMATION_MESSAGE);
+		
 		cardPanel.update(p);
 		game.setSize(900, 900);
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
